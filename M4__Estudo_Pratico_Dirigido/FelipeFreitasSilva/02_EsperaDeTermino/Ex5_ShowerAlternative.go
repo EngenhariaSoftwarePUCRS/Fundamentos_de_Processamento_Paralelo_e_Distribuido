fa60f8c2e477dc0790c63c0b11c8e4a90ba51fb0
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-09-08 18:21:24 -0300

    Estudo Prático Dirigido - 2 - Espera de Termino - Revisão

--- M4__Estudo_Pratico_Dirigido/FelipeFreitasSilva/02_EsperaDeTermino/Ex5_ShowerAlternative.go
+++ M4__Estudo_Pratico_Dirigido/FelipeFreitasSilva/02_EsperaDeTermino/Ex5_ShowerAlternative.go
@@ -1,12 +1,12 @@
 /*
 	* Felipe Freitas Silva
 	* 01/09/2023
-	
+ 
 	* 1) Esta é uma solução para a questão anterior?
-	* R: 
+	* R: Sim, visto que sincroniza a leitura com a escrita, e só permite o termino ao escrever no canal quit, após escrever e ler todos os valores
 
 	* 2) O que garante que todos os valores serão lidos antes do final do programa?
-	* R: 
+	* R: A sincronização citada acima
 */
 
 package main
@@ -15,15 +15,15 @@ import "fmt"
 
 func main() {
 	ch := make(chan int)
-	quit := make(chan bool)
+	quit := make(chan struct{})
 	go shower(ch, quit)
 	for i := 0; i < 10; i++ {
 		ch <- i
 	}
-	quit <- false // or true, does not matter
+	quit <- struct{}{}
 }
 
-func shower(c chan int, quit chan bool) {
+func shower(c <-chan int, quit <-chan struct{}) {
 	for {
 		select {
 		case j := <-c:
