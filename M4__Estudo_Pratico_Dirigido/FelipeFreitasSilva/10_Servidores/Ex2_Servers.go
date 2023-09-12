b5ac631a98c3f0c1591e1c8e9827123f0d7ecd20
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-09-12 11:52:31 -0300

    Estudo Prático Dirigido - 10 - Server - Finalizado

--- M4__Estudo_Pratico_Dirigido/FelipeFreitasSilva/10_Servidores/Ex2_Servers.go
+++ M4__Estudo_Pratico_Dirigido/FelipeFreitasSilva/10_Servidores/Ex2_Servers.go
@@ -1,12 +1,12 @@
 /*
 	* Felipe Freitas Silva
-	* 08/09/2023
+	* 08/09/2023 - 12/09/2023
 
 	* 1) Quantos clientes podem estar sendo tratados concorrentemente?
 	* R: Podem ser tratados tantos clientes quanto a capacidade de processamento do servidor permitir.
 
 	* 2) Agora suponha que o seu servidor pode estar tratando no máximo 10 clientes concorrentemente. Como você faria?
-	* R: Criaria uma variável que guarda o valor da quantidade de processos ativos em um momento do tempo, que é incrementada toda vez que um processo (cliente) é chamado e decrementada ao enviar a resposta para o cliente (Olhar Código)
+	* R: Criaria um canal com capacidade de 10 clientes, e a cada requisição, o servidor adicionaria um cliente ao canal, e quando o cliente terminasse de ser atendido, o servidor removeria o cliente do canal, o que permite que no máximo 10 clientes sejam atendidos ao mesmo tempo, enquanto os outros ficam "na fila", esperando para escrever no canal.
 */
 
 package main
@@ -16,59 +16,112 @@ import (
 	"math/rand"
 )
 
-const LOG = false
+const (
+	// Upper limit of clients
+	CLIENT_AMOUNT = 100
+	// Greatest amount of concurrent client requests
+	CLIENT_POOL = 10
+	// Show display messages regarding the processess
+	LOG = false
+)
 
 type Request struct {
-	v      int
-	ch_ret chan int
+	value   int
+	retChan	chan int
 }
 
-func cliente(i int, req chan Request) {
-	var v, r int
-	clientChan := make(chan int)
-	for {
-		v = rand.Intn(1000)
-		req <- Request{v, clientChan}
-		r = <-clientChan
+type Client struct {
+	id int
+	mainChan chan int
+}
+
+// Runs the client, sending requests and receiving responses
+// 
+// If a limit is passed, the client will stop after sending that amount of requests
+func (c Client) Run(req chan Request, limit... int) {
+	while := func(i int) bool {
+		if len(limit) > 0 {
+			return i < limit[0]
+		}
+		return true
+	}
+	for i, value, res := 0, 0, 0; while(i); i++ {
+		value = rand.Intn(1000)
+		req <- Request{value, c.mainChan}
+		res = <-c.mainChan
 		if LOG {
-			fmt.Println("cli: ", i, " req: ", v, "  resp:", r)
+			fmt.Println("cli: ", c.id, " req: ", value, "  resp:", res)
 		}
 	}
 }
 
-func handleRequest(id int, req Request, currentConcurrentProcesses *int) {
+type Server struct {
+	mainChan chan Request
+	currentConcurrentRequests *int
+	maxConcurrentRequests int
+	concurrentRequestsHandler chan struct{}
+}
+
+func NewServer(mainChan chan Request, maxConcurrentRequests int) Server {
+	currentConcurrentRequests := 0
+	return Server{
+		mainChan:                  mainChan,
+		currentConcurrentRequests: &currentConcurrentRequests,
+		maxConcurrentRequests:     maxConcurrentRequests,
+		concurrentRequestsHandler: make(chan struct{}, maxConcurrentRequests),
+	}
+}
+
+// Runs the server, receiving requests and handling them
+// 
+// If a limit is passed, the server will stop after handling that amount of requests
+func (s Server) Run(limit... int) {
+	while := func(i int) bool {
+		if len(limit) > 0 {
+			return i < limit[0]
+		}
+		return true
+	}
+	for i := 0; while(i); i++ {
+		if LOG {
+			fmt.Println("Current Concurrent Requests: ", *s.currentConcurrentRequests)
+		}
+		req := <-s.mainChan
+		s.AddConcurrentRequest()
+		go s.handleRequest(i, req)
+	}
+}
+
+// Builds up to MAX(10) concurrent requests and then waits until one is free
+func (s Server) AddConcurrentRequest() {
+	(*s.currentConcurrentRequests)++
+	s.concurrentRequestsHandler <- struct{}{}
+}
+
+// Frees one of the concurrent requests
+func (s Server) RemoveConcurrentRequest() {
+	<-s.concurrentRequestsHandler
+	(*s.currentConcurrentRequests)--
+}
+
+// Receives a request and returns it's value doubled
+func (s Server) handleRequest(id int, req Request) {
 	if LOG {
 		fmt.Println("                                 handleRequest ", id)
 	}
-	req.ch_ret <- req.v * 2
-	*currentConcurrentProcesses--
-}
-
-func servidorConc(in chan Request, maxConcurrentClients int) {
-	currentConcurrentProcesses, j := 0, 0
-	for {
-		if currentConcurrentProcesses < maxConcurrentClients {
-			currentConcurrentProcesses++
-			if LOG {
-				fmt.Println("Current concurrent processes: ", currentConcurrentProcesses)
-			}
-			req := <-in
-			j++
-			go handleRequest(j, req, &currentConcurrentProcesses)
-		}
-	}
+	req.retChan <- req.value * 2
+	s.RemoveConcurrentRequest()
 }
 
 func main() {
-	const (
-		CLIENT_AMOUNT = 100
-		CLIENT_POOL = 10
-	)
 	fmt.Println("------ Servidores - Criação dinâmica -------")
-	serverChan := make(chan Request) // CANAL POR ONDE SERVIDOR RECEBE PEDIDOS
-	go servidorConc(serverChan, CLIENT_POOL)      // LANÇA PROCESSO SERVIDOR
-	for i := 0; i < CLIENT_AMOUNT; i++ {      // LANÇA DIVERSOS CLIENTES
-		go cliente(i, serverChan)
+	server := NewServer(make(chan Request), CLIENT_POOL)
+	go server.Run()
+	for i := 1; i <= CLIENT_AMOUNT; i++ {
+		client := Client{i, make(chan int)}
+		go client.Run(server.mainChan, 10)
 	}
+
+	// Runs indefinetly
 	<-make(chan struct{})
 }
